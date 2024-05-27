6d16f8c766df9b8a0a6641182f00ec96bd1c1536
Hyeon-geun <hyeongeun.park@qureator.com>
2024-05-27 14:12:36 +0900

    update version

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     packages=['', 'omero.plugins'],
     package_dir={"": "src"},
     name="omero-cli-transfer",
-    version='0.5.0',
+    version='0.5.1',
     maintainer="Erick Ratamero",
     maintainer_email="erick.ratamero@jax.org",
     description=("A set of utilities for exporting a transfer"
@@ -101,7 +101,7 @@ setup(
         'lxml==4.9.4',
         'pydantic==1.10.13',
         'omero-acquisition-transfer @ '
-        'git+https://git@github.com/Qureator/omero-acquisition-transfer.git#egg=omero-acquisition-transfer-0.0.2',
+        'git+https://git@github.com/Qureator/omero-acquisition-transfer.git#egg=omero-acquisition-transfer-1.0.0',
     ],
     extras_require={
         "rocrate": ["rocrate==0.7.0"],
